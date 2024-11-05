79ee017c287a394b012c0b40de849a2cd82a0460
naman <parmarnaman19@gmail.com>
2024-11-05 18:33:52 +0530

    first

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function upload() {
+	return (
+		<SafeAreaView>
+			<Text>A upload page che</Text>
+		</SafeAreaView>
+	);
+}
---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function list() {
+	return (
+		<SafeAreaView>
+			<Text>A list page che</Text>
+		</SafeAreaView>
+	);
+}
